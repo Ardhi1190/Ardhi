586a6e69136740d7b844701bb3ebd94089490910
Ardhi1190 <ardhipratamaputra13@gmail.com>
2025-02-22 09:50:13 +0700

    Update Analisa_SE_2025.py

--- Analisa_SE_2025.py
+++ Analisa_SE_2025.py
@@ -13,7 +13,7 @@ st.title("📊 Dashboard Analisis Item Survey Employee Happiness & Engagement")
 
 # URL Google Sheets dalam format CSV
 sheet_url = "https://docs.google.com/spreadsheets/d/1V_wGUbLyDn6Uo5_EyFeLRp4AgZiYB72csQQJJEg5Yn8/export?format=csv"
-
+@st.cache_data(ttl=30)  # Cache berlaku selama 30 Detik
 def load_google_sheets(url):
     response = requests.get(url, verify=False)  # Hapus verify=False untuk keamanan
     if response.status_code == 200:
